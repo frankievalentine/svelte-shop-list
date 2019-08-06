f90daa356bf819a9c1c229138ec720c139eead98
frankievalentine <frankievalentine9@gmail.com>
2019-08-06 01:54:27 -0700

    Render list

--- src/App.svelte
+++ src/App.svelte
@@ -1,11 +1,17 @@
 <script>
-	export let name;
+	let items = [
+		{ id: 1, name: "Milk", done: false },
+		{ id: 2, name: "Bread", done: true },
+		{ id: 3, name: "Eggs", done: false },
+	]
 </script>
 
-<style>
-	h1 {
-		color: purple;
-	}
-</style>
+<div>
+	<h1>Things to buy</h1>
 
-<h1>Hello {name}!</h1>
+	<ul>
+	 {#each items as item}
+	 	<li>{item.name}</li>
+	 {/each}
+	</ul>
+</div>
